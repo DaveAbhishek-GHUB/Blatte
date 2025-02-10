9cb0cece5af5c4cf74d34c4b32543c6ce693b980
Abhishek <abhishek.dtridhyatech@gmail.com>
2025-02-10 12:26:58 +0530

    fix: dashboard bug fixed.

--- static/javascripts/admins/dashboard.js
+++ static/javascripts/admins/dashboard.js
@@ -2,6 +2,7 @@ function showContent(contentId) {
     // Hide all content sections
     document.getElementById('dashboard-content').style.display = 'none';
     document.getElementById('insertProducts-content').style.display = 'none';
+    document.getElementById('usersInformation-content').style.display = 'none';
     
     // Show the selected content section
     document.getElementById(contentId + '-content').style.display = 'block';
